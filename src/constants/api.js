272f967d6259fe8a069ea28bbc842c8e52c49191
deleonardorabelo <leonardomrabelo@gmail.com>
2020-07-13 18:04:49 -0300

    fixed constant API

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,3 @@
 export const API_DOMAIN = 'http://192.168.25.139:21068';
+export const API_MAIN = 'https://carpede-negocios.herokuapp.com';
 export const STORE_ID = '5ef75a9500c9c90017395b20';
-export const STORE_NAME = 'CARPEDE';
--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-import { API_DOMAIN } from '../constants/api';
+import { API_DOMAIN, API_MAIN } from '../constants/api';
 
 export const api = axios.create({
   baseURL: API_DOMAIN,
 });
 
 export const apiMain = axios.create({
-  baseURL: 'http://192.168.25.139:21022', // https://carpede-negocios.herokuapp.com',
+  baseURL: API_MAIN,
 });
